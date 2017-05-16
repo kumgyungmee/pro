7fc2071dd7b248dc3d1220fff3fd25f74ba7f833
kumgyungmee <rudal2593@naver.com>
2017-05-16 20:35:57 +0900

    2017-5-16
    2017-5-16

--- source.c
+++ source.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-void main() {
 
+void _gugudan();
+void main() {
+	_gugudan();
 	printf("Hello GIT!");
 
-	return; 
+	return;
+}
+void _gugudan(){
+	int i, j;
+
+	for (i = 1; i < 10; i++){
+		for (j = 1; j < 10; j++){
+			printf("%d*%d=%d\n", j, i, i*j);
+		}
+	}
 }
