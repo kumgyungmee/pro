c7d6c01bac3aa984bd6c669eb0dd371ae726ff1f
kumgyungmee <rudal2593@naver.com>
2017-05-16 20:39:58 +0900

    Revert "2017-5-16"
    This reverts commit 7fc2071dd7b248dc3d1220fff3fd25f74ba7f833.

--- source.c
+++ source.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
 
-
-void _gugudan();
 void main() {
-	_gugudan();
+
 	printf("Hello GIT!");
 
-	return;
-}
-void _gugudan(){
-	int i, j;
-
-	for (i = 1; i < 10; i++){
-		for (j = 1; j < 10; j++){
-			printf("%d*%d=%d\n", j, i, i*j);
-		}
-	}
+	return; 
 }
